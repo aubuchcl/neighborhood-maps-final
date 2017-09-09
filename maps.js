5ba7f1eceb012b97ced220762b1dc93f5de4b26b
Christopher Aubuchon <aubuchcl@gmail.com>
2017-09-09 00:46:45 -0700

    refactored out funcitons from loops

--- maps.js
+++ maps.js
@@ -18,7 +18,17 @@ function initMap() {
       center: tahoe,
       zoom: 13
     });
+    var locationMarker = function(x, marker){
+        locations[x].marker = marker;
+    }
 
+    var markerListener = function(marker){
+
+        marker.addListener('click', function() {
+            console.log(this);
+            populateInfoWindow(this, largeInfowindow);
+        });
+    }
     var largeInfowindow = new google.maps.InfoWindow();
     var bounds = new google.maps.LatLngBounds();
     // The following group uses the location array to create an array of markers on initialize.
@@ -38,14 +48,16 @@ function initMap() {
       markers.push(marker);
 
       //bind markers to each location object
-      (function(i){locations[i].marker = marker;}(i));
+      // (function(i){locations[i].marker = marker;}(i));
+      locationMarker(i, marker);
 
       // Create an onclick event to open an infowindow at each marker.
 
-      marker.addListener('click', function() {
-        console.log(this);
-        populateInfoWindow(this, largeInfowindow);
-      });
+      // marker.addListener('click', function() {
+      //   console.log(this);
+      //   populateInfoWindow(this, largeInfowindow);
+      // });
+      markerListener(marker);
       bounds.extend(markers[i].position);
     }
     // Extend the boundaries of the map for each marker
