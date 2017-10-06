8307d41b3e505220ca60e8edd7123c05ab282b77
Christopher Aubuchon <aubuchcl@gmail.com>
2017-10-06 11:02:58 -0700

    removed unneeded loop

--- js/viewmodel.js
+++ js/viewmodel.js
@@ -58,7 +58,7 @@ function AppViewModel(){
     });
 
     self.eventClickWindow = function() {
-        markers.forEach(function(marker){marker.setAnimation(null);});
+       // markers.forEach(function(marker){marker.setAnimation(null);});
         largeInfowindow = new google.maps.InfoWindow();
         for(var i = 0; i < markers.length; i++){
             if(this.title == markers[i].title){
